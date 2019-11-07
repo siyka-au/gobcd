074cfd46744e9d7b0d90a2cbb31a61abf9873237
piratecarrot <39475419+piratecarrot@users.noreply.github.com>
2019-11-07 23:21:27 +1100

    Fixed byte order issue in test

--- codec_test.go
+++ codec_test.go
@@ -10,5 +10,5 @@ func TestEncode(t *testing.T) {
 	var i uint64
 	i = 12345678
 	b := Encode(i)
-	assert.Equal(t, []byte{0x12, 0x34, 0x56, 0x78}, b, "")
+	assert.Equal(t, []byte{0x78, 0x56, 0x34, 0x12}, b, "")
 }
