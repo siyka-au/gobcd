23296dca1e27ec2c0bc41e1f2e0cd9aa0c4d0daf
Michael Alleblas <malleblas@siyka.com>
2019-11-07 23:15:51 +1100

    Working on new algorithm

---
+++ codec_test.go
@@ -0,0 +1,14 @@
+package bcd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncode(t *testing.T) {
+	var i uint64
+	i = 12345678
+	b := Encode(i)
+	assert.Equal(t, []byte{0x12, 0x34, 0x56, 0x78}, b, "")
+}
